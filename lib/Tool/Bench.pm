949a08d55cfdecca77f5d87443aa2a0af5c4f472
ben hengst <notbenh@cpan.org>
2009-10-08 22:42:25 -0700

    was thinking that the resutls needed to be there own object, but the more that I though about it that was a bad idea and we will just store things as a stack for now

--- lib/Tool/Bench.pm
+++ lib/Tool/Bench.pm
@@ -1,11 +1,23 @@
 package Tool::Bench;
+use Moose;
+use Scalar::Util qw{looks_like_number};                                                                                                                               
+use List::Util qw{min max};
+use Benchmark::Stopwatch::Pause;
 
-use warnings;
-use strict;
+has results => (
+   metaclass => 'Collection::Array',
+   is        => 'ro',
+   isa       => 'ArrayRef',
+   lazy      => 1,
+   default   => sub { [] },
+   provides  => {
+      push   => 'add_result',
+   }
+);
 
 =head1 NAME
 
-Tool::Bench - The great new Tool::Bench!
+Tool::Bench - Stuff to make Benchmarking easy.
 
 =head1 VERSION
 
@@ -54,11 +66,7 @@ ben hengst, C<< <notbenh at cpan.org> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-tool-bench at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Tool-Bench>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
+Please post bugs to github L<http://github.com/notbenh/tool_bench/issues>.
 
 
 =head1 SUPPORT
@@ -67,36 +75,24 @@ You can find documentation for this module with the perldoc command.
 
     perldoc Tool::Bench
 
-
 You can also look for information at:
 
 =over 4
 
-=item * RT: CPAN's request tracker
+=item * Project hosted at
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Tool-Bench>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Tool-Bench>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Tool-Bench>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Tool-Bench/>
+L<http://github.com/notbenh/tool_bench>
 
 =back
 
 
 =head1 ACKNOWLEDGEMENTS
 
+Thanks goes to Eric Wilhelm and Jonathan Leto, and the entire euler_bench team for making this possible.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009 ben hengst.
+Copyright 2009
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
@@ -107,4 +103,7 @@ See http://dev.perl.org/licenses/ for more information.
 
 =cut
 
-1; # End of Tool::Bench
+__PACKAGE__->meta->make_immutable();
+no Moose;
+
+1;
