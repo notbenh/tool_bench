f3b757598a9d13a8262ac1b6a91cd4b920993688
ben hengst <notbenh@cpan.org>
2009-11-06 13:21:25 -0800

    more Command work

--- lib/Tool/Bench/Command.pm
+++ lib/Tool/Bench/Command.pm
@@ -65,7 +65,7 @@ has [map{sprintf q{_build_%s}, $_} DATA ] => (
    default => '',
 );
 
-has [map{sprintf q{%s_validate'},$_} EVENTS ] => (
+has [map{sprintf q{%s_validate},$_} EVENTS ] => (
    is => 'rw',
    isa => 'CodeRef',
    lazy_build => 1,
@@ -75,35 +75,62 @@ has [map{sprintf q{_build_%s_validate}, $_} EVENTS ] => (
    is => 'rw',
    isa => 'CodeRef',
    lazy => 1,
-   default => sub{1},
+   default => sub{sub{1}},
 );
+
+=head2 cmd
+
+=cut
+sub cmd {
+   my $self = shift;
+   return ($self->has_command)                        ? $self->command
+        : ($self->has_interpreter && $self->has_file) ? join ' ', $self->interpreter, $self->file
+        :                                               undef ;
+};
 
 =head2 run
 
 =cut
-
 sub run {
    my $self = shift;
    my $num_of_runs = shift || 1;
 
-   if (length($self->setup) > 0) {
-      my $rv = qx{$self->setup};
-      die '!!! SETUP FAILED TO VALIDATE' unless $self->setup_validate->($rv);
-   }
-
-   my $cmd = ($self->has_command)                        ? $self->command
-           : ($self->has_interpreter && $self->has_file) ? join ' ', $self->interpreter, $self->file
-           :                                               undef ;
+   $self->run_setup;
+   my $cmd = $self->cmd;
 
    foreach (1..$num_of_runs) {
       $self->time_run($cmd);
    }
 
+   $self->run_cleanup;
+
+   return $self->result->count;
+}
+
+=head2 run_setup
+
+=cut
+
+sub run_setup {
+   my $self = shift;
+   if (length($self->setup) > 0) {
+      my $cmd = $self->setup;
+      my $rv  = qx{$cmd};
+      die '!!! SETUP FAILED TO VALIDATE' unless $self->setup_validate->($rv);
+   }
+}
+
+=head2 run_cleanup
+
+=cut
+
+sub run_cleanup {
+   my $self = shift;
    if (length($self->cleanup) > 0) {
-      my $rv = qx{$self->setup};
+      my $cmd = $self->cleanup;
+      my $rv  = qx{$cmd};
       die '!!! CLEANUP FAILED TO VALIDATE' unless $self->cleanup_validate->($rv);
    }
-   return $self->result->count;
 }
 
 =head2 time_run
@@ -112,15 +139,15 @@ sub run {
 
 sub time_run {
    my $self = shift;
-   my $cmd  = shift;
+   my $cmd  = shift || $self->cmd;
    die '!!! NO COMMAND SPECIFIED' unless defined $cmd;
 
    $self->sw->unpause($cmd);
    my $rv = qx{$cmd};
    $self->sw->pause;
-use Data::Dumper;
-print Dumper($self->sw->as_unpaused_data);
-   $self->result->add(pop @{[map{$_->{elapsed_time}} @{$self->sw->as_unpaused_data->{laps}}]} );
+
+   $self->result->add(pop @{[map{$_->{elapsed_time}} @{$self->sw->as_unpaused_data->{laps}}]} )
+      if $self->run_validate->($rv);
 }
 
 
--- t/03-command.t
+++ t/03-command.t
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use Test::Most qw{no_plan};
+#use Carp::Always;
 
 #-----------------------------------------------------------------
 #  
@@ -13,10 +14,13 @@ BEGIN {
    use_ok('Tool::Bench::Command');
    can_ok('Tool::Bench::Command', qw{
       result
-      run
       setup 
-      run 
+      setup_validate
+      run
+      time_run
+      run_validate
       cleanup
+      cleanup_validate
    });
 
 }
@@ -32,7 +36,33 @@ isa_ok(  $c,
 
 ok( $c->interpreter('ls'), q{interp} );
 ok( $c->file('.'), q{file} );
-
 eq_or_diff( $c->result->count, 0 , q{result empty} );
 ok( $c->run, q{run} );
+eq_or_diff( $c->result->count, 1 , q{result} );
+ok( $c->run_validate(sub{0}), q{plug in run_validate FAIL} );
+ok( $c->run, q{run} );
+eq_or_diff( $c->result->count, 1 , q{result unchanged, FAILED VALIDATE} );
+ok( $c->run_validate(sub{1}), q{plug in run_validate PASS} );
+ok( $c->run, q{run} );
+eq_or_diff( $c->result->count, 2 , q{result update} );
+
+#---------------------------------------------------------------------------
+#  SIMPLE Workflow example:
+#---------------------------------------------------------------------------
+ok( $c = Tool::Bench::Command->new(
+            setup => 'echo "hello" >> /tmp/testme',
+            setup_validate => sub{-e '/tmp/testme'},
+            cleanup => 'rm /tmp/testme',
+            cleanup_validate => sub{!-e '/tmp/testme'},
+            interpreter => 'cat',
+            file => '/tmp/testme',
+            run_validate => sub{$_=shift;m/hello/},
+         ),
+      q{workflow setup},
+);
+
+ok( $c->run(10), q{run} );
+is( $c->result->count, 10, q{run count = result count} );
+ok(!$c->time_run, q{run alone fails cleanup already run} );
+is( $c->result->count, 10, q{run count = result count} );
 
